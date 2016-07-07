318e4dacb12846610f59e9ce68bcf62dfca27709
Administrator <admin@example.com>
2016-07-07 01:25:52 -0500

    update autoconfig conf

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,7 @@ class nextcloud (
   $dbpass         = 'nextcloud',
   $dbhost         = 'localhost',
   $dbname         = 'nextcloud',
-  $dbtype         = 'pgsql',
-  $datadir        = '/srv/nextclouddata',
+  $dbtype         = 'mysql',
   $install_file   = 'nextcloud-9.0.52.tar.bz2',
   $install_url    = "https://download.nextcloud.com/server/releases/${install_file}",
   $fresh_install  = true,
@@ -12,17 +11,18 @@ class nextcloud (
   $manage_db      = true,
   $manage_apache  = true,
   $docroot        = '/var/www/html',
+  $datadir        = "${docroot}/nextcloud/data",
   $import_db      = false,
 ) {
   include nextcloud::install
-#  $autoconfig = template('owncloud/autoconfig.php.erb')
-#  exec { 'setup-autoconfig':
-#    command => "echo \'${autoconfig}\' > /var/www/owncloud/config/autoconfig.php",
-#    user    => 'www-data',
-#    onlyif  => "test ! -f /var/www/owncloud/config/config.php",
-#    path    => ['/bin','/sbin','usr/local/bin','/usr/local/sbin','/usr/bin'],
-#    require => Package['owncloud'],
-#    notify  => Service['apache2'],
-#  }
+  $autoconfig = template('nextcloud/autoconfig.php.erb')
+  exec { 'setup-autoconfig':
+     command => "echo \'${autoconfig}\' > ${docroot}/nextcloud/config/autoconfig.php",
+     user    => 'apache',
+     onlyif  => "test ! -f ${docroot}/nextcloud/config/config.php",
+     path    => ['/bin','/sbin','usr/local/bin','/usr/local/sbin','/usr/bin'],
+     require => Class['nextcloud::install'],
+     notify  => Service['httpd'],
+   }
 
 }
